809a9db229f8cac2fefe8215853932572fdb24de
Santiago Andrés Serrano Vacca <72269728+santiago-a-serrano@users.noreply.github.com>
2021-05-25 23:31:32 -0500

    First commit

---
+++ Tweet-Details-Project/db.js
@@ -0,0 +1,8 @@
+module.exports = function() {
+  return {
+    replies: require('./replies.json'),
+    tweet: require('./tweet.json'),
+    retweeted_tweet: require('./retweeted_tweet.json'),
+    fundraising_hashtags: require('./fundraising_hashtags.json')
+  }
+}
